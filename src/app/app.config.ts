e402ed3d45e859db9cd271aab4e079719308b140
acanojiDev <aantoniiocano@gmail.com>
2026-03-22 13:42:41 +0100

    Revert "rename: environemnt.ts"
    This reverts commit dbe32eed5d52bc577886ea79876fe0afe11d5ea7.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,7 +19,7 @@ import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 import { ENVIRONMENT } from '@core/tokens/environment.token';
-import { environment } from '@env/environment';
+import { environment } from '@env/environments';
 
 const SocialPreset = definePreset(Aura, {
   semantic: {
