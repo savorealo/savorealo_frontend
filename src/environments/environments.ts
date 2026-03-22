14f2c9d93989de6e104cc9e2f46fb1be09e6799b
acanojiDev <aantoniiocano@gmail.com>
2026-03-22 13:44:23 +0100

    Revert "auth first steps: guards, routes, tokens, environments and supabaseservice created"
    This reverts commit 2f513d727a2f6669a190ff538da395c69ff929ea.

--- src/environments/environments.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-	production: false,
-	supabaseUrl: 'https://onveiokuaetylptwwsjh.supabase.co',
-	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9udmVpb2t1YWV0eWxwdHd3c2poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1MDg1OTEsImV4cCI6MjA4MDA4NDU5MX0.ftOHCAlQDReDr5LHBtgxv7WEzasIjByrGoPCFaUEBWI',
-	apiUrl: 'https://tfg-let-me-cook-backend.onrender.com',
-}
